994f7df7763d7ea5fb78771634663769c52c39b7
Robert5110 <odfkgpokfgphfpodh@gmail.com>
2025-02-21 11:12:18 -0300

    funções de cada um

--- db.sql
+++ db.sql
@@ -1,12 +1,24 @@
 --Criação e seleção do banco de dados
-CREATE D8 queimaki;
-SELECT D8 queimaki;
+CREATE DB queimaki;
+SELECT DB queimaki;
 
---Criação da tabela usuario
+--Criação da tabela usuario(Robert)
 CREATE TABLE user
 
---Criar a tabela de administrador(Robert)
+--Criar a tabela de administrador(Kauã)
 CREATE TABLE admin
 
---Criar a tebela localidade(GG)
-CREATE TABLE locali
+--Criar a tabela categorias(GG)
+CREATE TABLE catego
+
+--Criar a tabela de fornecedor(GG)
+CREATE TABLE forne
+
+--Criar a tabela produtos(Marcelo)
+CREATE TABLE produ
+
+--Criar a tabela contato(Matheus)
+CREATE TABLE cont
+
+--Criar a tabela de promoção(Luiz)
+CREATE TABLE promo
