dda1fde77228a13734a0d23fc674c89b7febcc83
Robert5110 <odfkgpokfgphfpodh@gmail.com>
2025-02-21 09:28:16 -0300

    Tabela de usuario

--- db.sql
+++ db.sql
@@ -10,3 +10,12 @@ CREATE TABLE admin
 
 --Criar a tebela localidade(GG)
 CREATE TABLE locali
+
+--Criar a tabela contato(Robert)
+CREATE TABLE cont
+
+--Criar a tabela categoria(GG)
+CREATE TABLE catego
+
+--Criar a tabela produto(Marcelo)
+CREATE TABLE produ
