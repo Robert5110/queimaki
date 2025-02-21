ecaca7cfeef28b61560e8514f1f3770d2765d062
Robert5110 <odfkgpokfgphfpodh@gmail.com>
2025-02-21 08:46:14 -0300

    afazeres DB

---
+++ db.sql
@@ -0,0 +1,12 @@
+--Criação e seleção do banco de dados
+CREATE D8 queimaki;
+SELECT D8 queimaki;
+
+--Criação da tabela usuario
+CREATE TABLE user
+
+--Criar a tabela de administrador(Robert)
+CREATE TABLE admin
+
+--Criar a tebela localidade(GG)
+CREATE TABLE locali
