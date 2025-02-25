9982acfc0351fe4312b4a1d3299ad8057b42bad0
Robert5110 <odfkgpokfgphfpodh@gmail.com>
2025-02-25 07:56:28 -0300

    afazeres

--- db.sql
+++ db.sql
@@ -3,10 +3,20 @@ CREATE DB queimaki;
 SELECT DB queimaki;
 
 --Criação da tabela usuario(Robert)
-CREATE TABLE user
+CREATE TABLE user(
+id_user not null auto_increment unique primary key,
+nome_user varchar(255) not null,
+email_user varchar(255),
+senha_user varchar(255),
+confirma_senha varchar(255)
+);
 
 --Criar a tabela de administrador(Kauã)
-CREATE TABLE admin
+CREATE TABLE admin(
+id_admin int not null unique primary key,
+nome_admin varchar(255) not null,
+CONSTRAINT fk_user_admin foreign key (id_user) references user(id_user)
+);
 
 --Criar a tabela contato(Luiz)
 CREATE TABLE cont
@@ -20,5 +30,5 @@ CREATE TABLE produ
 --Criar a tabela de fornecedor(GG)
 CREATE TABLE forne
 
---Criar a tabela de promoções(Luiz)
+--Criar a tabela de promoções(Matheus)
 CREATE TABLE promo
