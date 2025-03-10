622502229e4c6db7082e018bcd5896f849e6ebea
Robert5110 <bsiqueira.dev@gmail.com>
2025-03-10 08:51:11 -0300

    códigod

---
+++ queimaki/db.sql
@@ -0,0 +1,79 @@
+--Criação e seleção do banco de dados
+CREATE database queimaki;
+SELECT database queimaki;
+
+--Criação da tabela usuario (Robert) concluido
+CREATE TABLE usuario(
+id_user not null auto_increment unique primary key,
+nome_user varchar(255) not null,
+email_user varchar(255),
+senha_user varchar(255),
+confirma_senha varchar(255)
+);
+
+--Criar a tabela de administrador (Kauã) concluido
+CREATE TABLE administrador(
+id_admin int not null unique primary key,
+nome_admin varchar(255) not null,
+email_admin varchar(255),
+senha_admin varchar (255);
+id_user int,
+ foreign key (id_user) references usuario(id_user)
+);
+
+--Criar a tabela de contato (Luiz) concluido
+CREATE TABLE contatos (
+    id_contato INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    nome_cont VARCHAR(255) NOT NULL,
+    email_cont VARCHAR(255),
+    telefone_cont VARCHAR(50),
+    id_forne INT  ,
+    FOREIGN KEY (id_forne) REFERENCES forne(id_forne) 
+);
+
+--Criar a tabela categoria (GG) concluido
+create table categoria(
+    id_categ INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    nome_categ VARCHAR(255) NOT NULL,
+    descricao TEXT
+);
+
+--Criar a tabela produto (Marcelo) concluido
+CREATE TABLE produtos (
+id_produ INT  PRIMARY KEY AUTO_INCREMENT,
+nome_produ VARCHAR(255) NOT NULL,
+descricao_produ varchar(255),
+preco_produ varchar(255) NOT NULL,
+id_categ INT,
+id_forne INT,
+FOREIGN KEY (id_categ) REFERENCES catego(id_categ),
+FOREIGN KEY (id_forne) REFERENCES forne(id_forne)
+);
+
+--Criar a tabela de fornecedor(GG) concluido
+create table fornecedor(
+    id_forne INT auto_increment PRIMARY KEY UNIQUE NOT NULL,
+    nome_forne VARCHAR(255) NOT NULL,
+    cnpj_forne VARCHAR(17) UNIQUE NOT NULL,
+    email_forne VARCHAR(255), 
+    telefone_forne VARCHAR(20)
+);
+
+--Criar a tabela de promoções(Matheus) concluido
+CREATE TABLE promocoes (
+  id_promo INT AUTO_INCREMENT PRIMARY KEY,
+  plataforma VARCHAR(100) NOT NULL,
+  tipo_promocao VARCHAR(100) NOT NULL,
+  desconto VARCHAR(255),
+  periodo_inicio_promo DATE,
+  periodo_fim_promo DATE
+);
+
+--Relacionamento entre as tabelas de produtos e promoções
+CREATE TABLE produtos_promocoes (
+    id_produ INT,
+    id_promo INT,
+    PRIMARY KEY (id_produ, id_promo),
+    FOREIGN KEY (id_produ) REFERENCES produtos(id_produ),
+    FOREIGN KEY (id_promo) REFERENCES promocoes(id_promo)
+);
---
+++ queimaki/query.sql
@@ -0,0 +1,38 @@
+--[URF001] Login de usuarios
+SELECT * FROM usuario 
+WHERE email_user = 'email@exemplo.com' AND senha_user = MD5('123');
+
+--[ARF008] Login de administradores
+SELECT * FROM administrador
+WHERE email_admin = 'exemplo@gmail.com' AND senha_admin = MD5('123'); 
+
+--[URF003] Pesquisar produtos 
+SELECT * FROM produtos
+WHERE nome_produto LIKE '%nome_pesquisado%';
+
+--[URF005] Pesquisar fornecedores
+SELECT * FROM fornecedor
+WHERE nome_forne LIKE '%nome_pesquisado%';
+
+--[ARF007] Pesquisar categorias
+SELECT * FROM categoria
+WHERE nome_categ LIKE '%nome_pesquisado%';
+
+--[URF004] Pesquisar usuarios
+SELECT * FROM usuario
+WHERE nome_user LIKE '%termo_de_pesquisa%' OR email_user LIKE '%termo_de_pesquisa%';
+
+-- Adicionar uma promoção no produto
+INSERT INTO produtos_promocoes (id_produ, id_promo)
+VALUES (4, 3);
+
+-- Relação entre produto e promoções
+SELECT
+    p.nome_produ, p.preco_produ, pr.plataforma, pr.tipo_promocao, pr.desconto
+FROM
+    produtos p
+JOIN
+    produtos_promocoes pp ON p.id_produ = pp.id_produ
+JOIN
+    promocoes pr ON pp.id_promo = pr.id_promo;
+    
