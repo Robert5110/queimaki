1159d362ddb1362223e50ef8e5bb73a8253c9111
Robert5110 <bsiqueira.dev@gmail.com>
2025-03-26 10:16:51 -0300

    codigos

---
+++ queimaki/demo/src/main/java/queimaki/demo/models/dtos/entitys/repos/AdministradorEntity.java
@@ -0,0 +1,49 @@
+package com.api.springapi.models.entitys;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "administrador")
+public class AdministradorEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_admin")
+    private Integer idAdmin;
+
+    @Column(name = "nome_admin", nullable = false)
+    private String nomeAdmin;
+
+    @Column(name = "email_admin")
+    private String emailAdmin;
+
+    @Column(name = "senha_admin")
+    private String senhaAdmin;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UsersEntity usuario; 
+
+    public AdministradorEntity() {
+    }
+}
+
+public class AdiministradorEntity {
+    
+}
---
+++ queimaki/demo/src/main/java/queimaki/demo/models/dtos/entitys/repos/UsuariosEntity.java
@@ -0,0 +1,43 @@
+package com.api.springapi.models.entitys;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "usuario")
+public class UsuariosEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user") 
+    private Long idUser; 
+
+    @Column(name = "nome_user", nullable = false) 
+    private String nomeUser; 
+
+    @Column(name = "email_user")
+    private String emailUser;
+
+    @Column(name = "senha_user") 
+    private String senhaUser; 
+
+    @Column(name = "confirma_senha") 
+    private String confirmaSenha; 
+
+
+    public UsersEntity() {
+    }
+}
